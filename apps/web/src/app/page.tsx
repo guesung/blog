15291115d29891eb0276ef1a4f7a91ca3cbc4091
Kuesung Park <gueit214@naver.com>
2024-01-04 16:40:30 +0900

    refactor(web) : src로 폴더 묶기

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,7 +3,8 @@ import styles from "./page.module.css";
 import { isServer } from "@repo/utils/isServer";
 
 export default function Page(): JSX.Element {
-  console.log(isServer);
+  if (!isServer) console.log(MouseEvent);
+  
   return (
     <main className={styles.main}>
       <Button appName="aa">Click</Button>
