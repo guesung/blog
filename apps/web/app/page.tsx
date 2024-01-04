13f07e828545b8ea171827b1668389a8619d8b56
Kuesung Park <gueit214@naver.com>
2024-01-04 14:54:13 +0900

    feat: isServer 유틸 함수

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,58 +1,12 @@
-import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
-import styles from "./page.module.css";
 import { Button } from "@repo/ui/button";
-import hello from "@repo/utils/hello";
-
-function Gradient({
-  conic,
-  className,
-  small,
-}: {
-  small?: boolean;
-  conic?: boolean;
-  className?: string;
-}): JSX.Element {
-  return (
-    <span
-      className={[
-        styles.gradient,
-        conic ? styles.glowConic : undefined,
-        small ? styles.gradientSmall : styles.gradientLarge,
-        className,
-      ]
-        .filter(Boolean)
-        .join(" ")}
-    />
-  );
-}
-
-const LINKS = [
-  {
-    title: "Docs",
-    href: "https://turbo.build/repo/docs",
-    description: "Find in-depth information about Turborepo features and API.",
-  },
-  {
-    title: "Learn",
-    href: "https://turbo.build/repo/docs/handbook",
-    description: "Learn more about monorepos with our handbook.",
-  },
-  {
-    title: "Templates",
-    href: "https://turbo.build/repo/docs/getting-started/from-example",
-    description: "Choose from over 15 examples and deploy with a single click.",
-  },
-  {
-    title: "Deploy",
-    href: "https://vercel.com/new",
-    description:
-      " Instantly deploy your Turborepo to a shareable URL with Vercel.",
-  },
-];
+import styles from "./page.module.css";
+import { isServer } from "@repo/utils/isServer";
 
 export default function Page(): JSX.Element {
-  hello();
-  return <main className={styles.main}></main>;
+  console.log(isServer);
+  return (
+    <main className={styles.main}>
+      <Button appName="aa">Click</Button>
+    </main>
+  );
 }
