f564e538f6f0bea9232900eb22261e97f9b17e9d
guesung <gueit214@naver.com>
2024-04-07 13:23:31 +0900

    feat: useQuery훅을 구현한다

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,1 +1,3 @@
 export {default as useHello} from './useHello'
+export {default as useError} from './useError'
+export {default as useQuery} from './useQuery'
---
+++ packages/hooks/src/useQuery.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { useError } from ".";
+
+interface useQueryProps<T> {
+  initialData: T;
+  fetchFn: () => Promise<T>;
+  trigger?: unknown;
+}
+
+export default function useQuery<T>({
+  initialData,
+  fetchFn,
+  trigger
+}: useQueryProps<T>) {
+  const setError = useError();
+  const [data, setData] = useState<T>(initialData);
+
+  useEffect(() => {
+    fetchFn()
+      .then(response => {
+        setData(response);
+      })
+      .catch(error => {
+        setError(error);
+      });
+  }, [trigger, setError]);
+
+  return { data };
+}
