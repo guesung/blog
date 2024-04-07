7ffb000cd6f3972104c3a3f4fbcca2093ad1b349
guesung <gueit214@naver.com>
2024-04-07 13:23:26 +0900

    feat: useError훅을 구현한다

---
+++ packages/hooks/src/useError.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+export default function useError() {
+  const [error, setError] = useState<Error | unknown>();
+
+  useEffect(() => {
+    if (!error) return;
+
+    if (error instanceof Error) throw error;
+    else throw new Error("알 수 없는 에러가 발생했습니다.");
+  }, [error]);
+
+  return setError;
+}
