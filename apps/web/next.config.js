1d14775e8840804c5a2dfba4deacdb94218912c5
guesung <gueit214@naver.com>
2024-04-10 11:28:58 +0900

    chore: next.config의 image를 설정한다

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ['@guesung/ui'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+    ],
+  },
 };
