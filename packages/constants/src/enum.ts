2ac15fba44e3c8cf28fd5984bd94815148f90883
guesung <gueit214@naver.com>
2024-04-10 14:16:45 +0900

    feat: 상수에 enum을 추가한다

---
+++ packages/constants/src/enum.ts
@@ -0,0 +1,9 @@
+export const enum DirectionEnum {
+  'horizontal',
+  'vertical',
+}
+
+export const enum VariantEnum {
+  'outline',
+  'filled',
+}
--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -1,1 +1,2 @@
 export * from './Mock';
+export * from './enum';
