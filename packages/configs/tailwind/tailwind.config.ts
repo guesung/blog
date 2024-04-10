742e925df1509bd26358dc895ff018946ed72e8c
guesung <gueit214@naver.com>
2024-04-10 11:00:57 +0900

    fix: tailwind fontSize를 수정하고, Config타입을 제거한다

--- packages/configs/tailwind/tailwind.config.ts
+++ packages/configs/tailwind/tailwind.config.ts
@@ -1,14 +1,13 @@
 import { pxToRemTailwind, colors, fontSize, theme } from './src/style';
-import type { Config } from 'tailwindcss';
 
-const config: Config = {
+const config = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       ...pxToRemTailwind,
-      ...fontSize,
       colors,
       theme,
+      fontSize,
     },
   },
   plugins: [],
