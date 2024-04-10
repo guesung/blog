0862536fd20cdf10ffe51b135a3768edfc7cbecb
guesung <gueit214@naver.com>
2024-04-10 14:45:16 +0900

    feat: PostList 레이아웃을 설정한다

--- apps/web/src/app/components/PostList.tsx
+++ apps/web/src/app/components/PostList.tsx
@@ -6,13 +6,26 @@ export default function PostList() {
   // TODO : Client Data Fetching
 
   return (
-    <div>
+    <div className="grid grid-cols-2 gap-20">
       <Card
         id="1"
         title="Title"
-        description="Description"
         date="2021-09-13"
-        tags={['React.js', 'Next.js']}
+        tagList={['React.js', 'Next.js']}
+        image="https://source.unsplash.com/random"
+      />
+      <Card
+        id="1"
+        title="Title"
+        date="2021-09-13"
+        tagList={['React.js', 'Next.js']}
+        image="https://source.unsplash.com/random"
+      />
+      <Card
+        id="1"
+        title="Title"
+        date="2021-09-13"
+        tagList={['React.js', 'Next.js']}
         image="https://source.unsplash.com/random"
       />
     </div>
