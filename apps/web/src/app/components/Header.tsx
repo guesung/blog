0ef13830905254de5841d686c1937707d6eaa3b4
guesung <gueit214@naver.com>
2024-04-10 13:00:14 +0900

    feat: Header를 구현한다

--- apps/web/src/app/components/Header.tsx
+++ apps/web/src/app/components/Header.tsx
@@ -1,3 +1,14 @@
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className="text-title2 flex justify-between py-5">
+      <div className="">
+        <span>Guesung.</span>
+      </div>
+      <div className="flex gap-10">
+        <span>Note</span>
+        <span>About</span>
+        <span>달</span>
+      </div>
+    </header>
+  );
 }
