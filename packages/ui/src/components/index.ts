adb3255ada0fd13f2f400f3285db19af6a84bcdd
guesung <gueit214@naver.com>
2024-04-14 15:30:16 +0900

    feat: Calendar를 Mock데이터로 구현한다

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Card } from './Card';
 export { default as Tag } from './Tag';
 export { default as Button } from './Button';
 export { default as LinkButton } from './LinkButton';
+export { default as Calendar } from './Calendar';
