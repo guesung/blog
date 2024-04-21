dfe320f0a84047023a5c2ee4c8b13555d5713760
guesung <gueit214@naver.com>
2024-04-21 13:00:59 +0900

    fix: remove Card component

--- apps/blog/src/app/components/PostList.tsx
+++ apps/blog/src/app/components/PostList.tsx
@@ -1,33 +1,7 @@
 'use client';
 
-import { Card } from '@guesung/ui';
-
 export default function PostList() {
   // TODO : Client Data Fetching
 
-  return (
-    <div className="grid grid-cols-2 gap-20">
-      <Card
-        id="1"
-        title="Title"
-        date="2021-09-13"
-        tagList={['React.js', 'Next.js']}
-        image="https://source.unsplash.com/random"
-      />
-      <Card
-        id="1"
-        title="Title"
-        date="2021-09-13"
-        tagList={['React.js', 'Next.js']}
-        image="https://source.unsplash.com/random"
-      />
-      <Card
-        id="1"
-        title="Title"
-        date="2021-09-13"
-        tagList={['React.js', 'Next.js']}
-        image="https://source.unsplash.com/random"
-      />
-    </div>
-  );
+  return <div className="grid grid-cols-2 gap-20"></div>;
 }
