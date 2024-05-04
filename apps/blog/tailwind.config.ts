b6148242f8073d9b11b530ffa344fa9cf93a22e4
guesung <guesung@elicer.com>
2024-05-04 20:15:42 +0900

    fix: fix the tailwind issue

--- apps/blog/tailwind.config.ts
+++ apps/blog/tailwind.config.ts
@@ -2,5 +2,6 @@ const config = require('@guesung/tailwind-config/tailwind.config');
 
 module.exports = {
   ...config,
+  content: ['./src/**/*.tsx'],
   plugins: [require('@tailwindcss/typography')],
 };
