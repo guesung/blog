21eb85eac0793812eb5276dc3766b5bf78db1bdf
guesung <gueit214@naver.com>
2024-05-05 15:52:02 +0900

    feat: setting next.config.,js

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -11,6 +11,9 @@ const nextConfig = {
       },
     ],
   },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = withContentlayer(nextConfig);
