9a7a37e4da5212a3c421476d3c6e990d8eaf08fb
guesung <gueit214@naver.com>
2024-05-05 12:11:53 +0900

    design: edit width

--- apps/blog/src/app/posts/[slug]/page.tsx
+++ apps/blog/src/app/posts/[slug]/page.tsx
@@ -10,7 +10,7 @@ export default function page({ params: { slug } }: pageProps) {
   const { title, date, body } = getPost({ slug });
 
   return (
-    <div className="max-w-1000 mx-auto">
+    <div className="max-w-760 mx-auto">
       <PostTitle title={title} date={date} />
       <PostContent body={body} />
     </div>
