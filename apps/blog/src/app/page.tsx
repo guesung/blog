82736e0617888d115c460de6af11eecba3773bfb
guesung <gueit214@naver.com>
2024-05-06 14:04:18 +0900

    design: change font size

--- apps/blog/src/app/page.tsx
+++ apps/blog/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <div className="max-w-1000 mx-auto">
       <div className="text-title1 my-20 text-center">ALL POSTS.</div>
-      <div className='grid grid-cols-1 lg:grid-cols-2 justify-items-center'>
+      <div className="grid grid-cols-1 justify-items-center lg:grid-cols-2">
         {postList.map(post => (
           <PostCard
             id={post._id}
--- apps/blog/src/app/posts/[slug]/components/PostTitle.tsx
+++ apps/blog/src/app/posts/[slug]/components/PostTitle.tsx
@@ -3,5 +3,5 @@ interface PostTitleProps {
   date: string;
 }
 export default function PostTitle({ title, date }: PostTitleProps) {
-  return <div className="text-title1 my-50 text-center">{title}</div>;
+  return <div className="text-title3 my-50 text-center">{title}</div>;
 }
