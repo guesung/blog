14f1d64e3a3b0c3575a7d690ca3a8b60cf10f3de
guesung <gueit214@naver.com>
2024-05-06 14:24:06 +0900

    feat: show title

--- apps/blog/src/app/page.tsx
+++ apps/blog/src/app/page.tsx
@@ -8,12 +8,7 @@ export default function Page() {
       <div className="text-title1 my-20 text-center">ALL POSTS.</div>
       <div className="grid grid-cols-1 justify-items-center lg:grid-cols-2">
         {postList.map(post => (
-          <PostCard
-            id={post._id}
-            url={post.url}
-            title={post.title}
-            date={post.date}
-          />
+          <PostCard url={post.url} title={post.title} date={post.date} />
         ))}
       </div>
     </div>
--- apps/blog/src/components/PostCard.tsx
+++ apps/blog/src/components/PostCard.tsx
@@ -1,17 +1,16 @@
 import Link from 'next/link';
 
 interface PostCardProps {
-  id: string;
   title: string;
   date: string;
   url: string;
 }
 
-export default function PostCard({ id, title, date, url }: PostCardProps) {
-  console.log(title, date, id);
+export default function PostCard({ title, date, url }: PostCardProps) {
   return (
     <Link href={url}>
-      <div className="h-300 w-400 rounded-16 relative my-10 bg-slate-500">
+      <div className="h-300 w-400 rounded-16 relative my-10 bg-slate-100">
+        <div className="text-subtitle2 p-20 leading-[1.2]">{title}</div>
         <span className="absolute bottom-12 right-12">{date}</span>
       </div>
     </Link>
