3491815496b7de9ab2140db5ef94f1b16e3a013e
guesung <gueit214@naver.com>
2024-05-15 11:14:31 +0900

    feat: add contents folder for `tailwindcss`

--- apps/blog/tailwind.config.ts
+++ apps/blog/tailwind.config.ts
@@ -2,7 +2,7 @@ const config = require('@guesung/tailwind-config/tailwind.config');
 
 module.exports = {
   ...config,
-  content: ['./src/**/*.tsx'],
+  content: ['./src/**/*.tsx', './contents/**/*.mdx'],
   plugins: [require('@tailwindcss/typography')],
   darkMode: 'selector',
 };
