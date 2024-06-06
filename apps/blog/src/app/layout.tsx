b1e6a2ebfd6b25e9150129391a2655492caeebba
guesung <gueit214@naver.com>
2024-06-06 12:51:02 +0900

    feat: add search advisor for naver and google

--- apps/blog/src/app/layout.tsx
+++ apps/blog/src/app/layout.tsx
@@ -17,6 +17,12 @@ const pretendard = localFont({
 export const metadata: Metadata = {
   title: 'Guesung Blog',
   description: 'Welcome to Guesung Blog!',
+  verification: {
+    google: 'ydZVzq0xbjyB0kO5I4Rmi1U-fB60L0eThb2a4K62KCo',
+    other: {
+      'naver-site-verification': '030114aa4592fdf3cee71d19dae3c4a36aa1bbe5',
+    },
+  },
 };
 
 export default function RootLayout({
