2dcf354ae2eed7e749a45f25f9ec1f4b86dd42e0
guesung <gueit214@naver.com>
2024-06-28 15:17:27 +0900

    feat: implement `FramerMotionVariants` constants

---
+++ packages/constants/src/FramerMotionVariants.ts
@@ -0,0 +1,9 @@
+export const VISIBILITY = {
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 },
+};
+
+export const MOVE = {
+  hidden: { x: -50, opacity: 0 },
+  visible: { x: 0, opacity: 1 },
+};
--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -3,3 +3,4 @@ export * from './enum';
 export * from './Date';
 export * from './URL';
 export * from './type';
+export * from './FramerMotionVariants';
