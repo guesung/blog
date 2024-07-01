cd1e681a54a771e0b9d24c4853551443068a010c
guesung <gueit214@naver.com>
2024-07-01 17:04:37 +0900

    feat: add google auth library and google spreadsheet

--- apps/study-log/src/app/page.tsx
+++ apps/study-log/src/app/page.tsx
@@ -1,52 +1,19 @@
-import { Table } from '../components';
+import { GoogleSpreadSheet } from '@components';
+import { getGoogleSpreadSheetData } from '@utils';
 
+export default async function Home() {
+  const data = await getGoogleSpreadSheetData();
+  if (!data) return null;
 
-type ColumnType = {
-  time: string;
-  todo: string;
-  goal: string;
-  feeling: string;
-  isCompleted: boolean;
-};
-const columnList = ['time', 'todo', 'goal', 'feeling', 'isCompleted'];
-const HEADER_LIST: ColumnType = {
-  time: '시간',
-  todo: '할 것',
-  goal: '목표를 이루기 위해 신경써야 할 것,',
-  feeling: '느낀 점',
-  isCompleted: false,
-};
-const DATA_LIST: ColumnType[] = [
-  {
-    time: '21:00',
-    todo: '스터디 로그 작성',
-    goal: '초집중하자.',
-    feeling: '너무 수고했다.',
-    isCompleted: false,
-  },
-  {
-    time: '21:00',
-    todo: '스터디 로그 작성',
-    goal: '초집중하자.',
-    feeling: '너무 수고했다.',
-    isCompleted: false,
-  },
-  {
-    time: '21:00',
-    todo: '스터디 로그 작성',
-    goal: '초집중하자.',
-    feeling: '너무 수고했다.',
-    isCompleted: false,
-  },
-];
+  const tableData = data.map(row => {
+    return {
+      time: row.get('time'),
+      todo: row.get('todo'),
+      goal: row.get('goal'),
+      feeling: row.get('feeling'),
+      isCompleted: row.get('isCompleted'),
+    };
+  });
 
-export default function Home() {
-  return (
-    <div className="flex justify-center">
-      <Table columnList={columnList}>
-        <Table.Header headerList={HEADER_LIST} />
-        <Table.Body dataList={DATA_LIST} contentEditable />
-      </Table>
-    </div>
-  );
+  return <GoogleSpreadSheet tableData={tableData} />;
 }
