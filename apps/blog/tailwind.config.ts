d10d8f83ff7d52a880f360dff654170f680ac2d6
guesung <gueit214@naver.com>
2024-07-19 13:51:25 +0900

    fix: tailwind무한 컴파일 이슈를 해결한다
    ref: https://github.com/tailwindlabs/tailwindcss/issues/5752

--- apps/blog/tailwind.config.ts
+++ apps/blog/tailwind.config.ts
@@ -2,12 +2,7 @@ const config = require('@guesung/tailwind-config/tailwind.config');
 
 module.exports = {
   ...config,
-  content: [
-    './contents/**/*.mdx',
-    './src/**/*.{js,ts,jsx,tsx}',
-    '../../packages/ui/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./contents/**/*.mdx', './src/**/*.{js,ts,jsx,tsx}'],
   plugins: [require('@tailwindcss/typography')],
   darkMode: 'selector',
-
 };
--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,1 +1,6 @@
-module.exports = require('@guesung/tailwind-config/tailwind.config');
+const config = require('@guesung/tailwind-config/tailwind.config');
+
+module.exports = {
+  ...config,
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+};
