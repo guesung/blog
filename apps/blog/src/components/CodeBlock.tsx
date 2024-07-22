71627261c9be870c66634ac8cb6c321948fe1f11
guesung <gueit214@naver.com>
2024-07-22 16:42:03 +0900

    revert: `CodeBlock`을 되돌린다

--- apps/blog/src/components/CodeBlock.tsx
+++ apps/blog/src/components/CodeBlock.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
-interface PageProps {
-  
-}
+'use client';
+import { PropsWithChildren, useRef } from 'react';
+import CopyIcon from '@svgs/copy.svg';
+import { copyToClipboard } from '@guesung/utils';
 
+export default function CodeBlock({ children }: PropsWithChildren) {
+  const ref = useRef<HTMLPreElement>(null);
+  const handleCopyClick = async () => {
+    const text = ref.current?.querySelector('code')?.innerText ?? '';
+    await copyToClipboard(text);
+  };
 
-export default function page({}:PageProps) {
-  const [number, setNumber] = React.useState(1)
-  return <div></div>;
+  return (
+    <div className="relative">
+      <pre data-theme="one-dark-pro" className="relative m-0" ref={ref}>
+        {children}
+        <div className="absolute bottom-20 right-20">
+          <CopyIcon
+            className="h-20 w-20 cursor-pointer"
+            onClick={handleCopyClick}
+          />
+        </div>
+      </pre>
+    </div>
+  );
 }
