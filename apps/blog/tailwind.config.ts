a6fe4dafd496c6e3923996332986ab5f9834de30
guesung <gueit214@naver.com>
2024-07-22 16:46:38 +0900

    refactor: tailwind를 tailwind-config패키지로 옮긴다

--- apps/blog/tailwind.config.ts
+++ apps/blog/tailwind.config.ts
@@ -2,8 +2,7 @@ import config from '@guesung/tailwind-config/tailwind.config';
 
 module.exports = {
   content: [
-    './contents/**/*.mdx',
-    './src/**/*.{js,ts,jsx,tsx}',
+    'src/**/*.{js,ts,jsx,tsx}',
     '../../packages/ui/src/**/*.{js,ts,jsx,tsx}',
   ],
   plugins: [require('@tailwindcss/typography')],
