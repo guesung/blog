cda6acc5d389a9b4264c2cef4e30e95fe41d9ff4
guesung <gueit214@naver.com>
2024-08-02 15:56:58 +0900

    fix: build에러를 해결한다

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -5,23 +5,8 @@ const nextConfig = {
   webpack: config => {
     config.module.rules.push({
       test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-        },
-        {
-          loader: 'file-loader',
-          options: {
-            name: 'static/[path][name].[ext]',
-          },
-        },
-      ],
-      type: 'javascript/auto',
-      issuer: {
-        and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
-      },
+      use: ['@svgr/webpack'],
     });
-
     return config;
   },
   images: {
