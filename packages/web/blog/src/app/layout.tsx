de567f21c7fd3b4728603ec35da65ca30ec6e84a
guesung <gueit214@naver.com>
2024-08-07 14:05:31 +0900

    feat: JsonLD를 분리한다

--- packages/web/blog/src/app/layout.tsx
+++ packages/web/blog/src/app/layout.tsx
@@ -8,6 +8,7 @@ import { cn } from '@guesung/utils';
 import localFont from 'next/font/local';
 import { Theme } from '@components/provider/ThemeProvider';
 import { GoogleAnalytics } from '@next/third-parties/google';
+import { JSON_LD_DATA } from '@constants/JsonLd';
 
 const pretendard = localFont({
   src: '../../public/font/PretendardVariable.woff2',
@@ -27,40 +28,6 @@ export const metadata: Metadata = {
   },
 };
 
-const jsonLdData = {
-  '@context': 'https://schema.org',
-  '@type': 'WebPage',
-  name: 'Guesung Blog',
-  description: '안녕하세요, 프론트엔드 개발자 박규성입니다.',
-  keywords: 'blog, frontend, javascript, react',
-  url: 'https://blog.guesung.site',
-  image:
-    'https://github.com/user-attachments/assets/5c8b9ef8-1a2b-4ecc-b191-4ae95d4c5a00',
-  author: {
-    '@type': 'Person',
-    name: 'https://www.linkedin.com/in/guesung',
-  },
-  datePublished: '2024-07-30T11:35:00+07:00',
-  dateModified: '2024-07-30T11:35:00+07:00',
-  publisher: {
-    '@type': 'Organization',
-    name: '박규성 기술 블로그',
-    logo: {
-      '@type': 'ImageObject',
-      url: 'https://github.com/user-attachments/assets/5c8b9ef8-1a2b-4ecc-b191-4ae95d4c5a00',
-    },
-  },
-  headline: 'Guesung Blog',
-  mainEntityOfPage: {
-    '@type': 'WebPage',
-    '@id': 'https://blog.guesung.site/series/posts/system',
-  },
-  articleSection: '시스템으로 성장하기',
-  articleBody: '시스템을 사랑한 개발자',
-  thumbnailUrl:
-    'https://blog.guesung.site/_next/image?url=%2Fcontents%2Fposts%2Fsystem%2Fcover.png&w=1200&q=75',
-};
-
 export default function RootLayout({
   children,
 }: {
@@ -77,10 +44,11 @@ export default function RootLayout({
       })}
     >
       <body className={pretendard.className}>
+        {/* JSON LD */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
-            __html: JSON.stringify(jsonLdData, null, 2),
+            __html: JSON.stringify(JSON_LD_DATA, null, 2),
           }}
         />
         <ThemeProvider initialTheme={theme}>
