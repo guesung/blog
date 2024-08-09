3ff05c948e42bf9162649592f59e9ce2b342eefd
guesung <gueit214@naver.com>
2024-08-09 21:13:26 +0900

    docs: 주석 설명을 추가한다

--- packages/web/blog/src/components/ToggleTheme.tsx
+++ packages/web/blog/src/components/ToggleTheme.tsx
@@ -7,21 +7,23 @@ import SunIcon from '#images/icons/sun.svg';
 
 export default function ToggleTheme() {
   const { theme, setTheme } = useTheme();
+
   const handleTheme = () => {
     const theme = getCookie('theme');
+
     if (theme === 'dark') {
-      setCookie('theme', 'light');
-      setTheme('light');
-      document.documentElement.classList.remove('dark');
+      setCookie('theme', 'light'); // 설정값 보존
+      setTheme('light'); // 리렌더링
+      document.documentElement.classList.remove('dark'); // html에 적용
     } else {
       setCookie('theme', 'dark');
-      document.documentElement.classList.add('dark');
       setTheme('dark');
+      document.documentElement.classList.add('dark');
     }
   };
 
   return (
-    <div className="cursor-pointer dark" onClick={handleTheme}>
+    <div className="cursor-pointer" onClick={handleTheme}>
       {theme === 'dark' ? (
         <SunIcon fill="white" width={16} height={16} />
       ) : (
