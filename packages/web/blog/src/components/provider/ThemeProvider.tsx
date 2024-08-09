027542225cc4b18baa6a09f2b836b1168aee66ab
guesung <gueit214@naver.com>
2024-08-09 21:14:12 +0900

    design: 다크모드 변환시 애니메이션을 추가한다

--- packages/web/blog/src/components/provider/ThemeProvider.tsx
+++ packages/web/blog/src/components/provider/ThemeProvider.tsx
@@ -34,7 +34,7 @@ export default function ThemeProvider({
       <div
         className={cn(
           { dark: theme === 'dark' },
-          'text-body3 bg-white-1 text-black-1 min-h-screen',
+          'text-body3 bg-white-1 text-black-1 min-h-screen transition-colors',
           className
         )}
       >
