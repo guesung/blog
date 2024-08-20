18233bffc68d7749cf412824190a57f6bb2219c4
guesung <gueit214@naver.com>
2024-08-20 14:28:01 +0900

    Refactor : 사용하지 않는 컴포넌트 및 페이지를 제거한다 (#3)
    * refactor: 사용하지 않는 페이지 및 컴포넌트를 제거한다
    
    * refactor: 사용하지 않는 번역 페이지를 제거한다

--- packages/web/blog/src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-interface PageProps {}
-
-//
-//
-//
-
-export default function page({}: PageProps) {
-  return <div></div>;
-}
--- packages/web/blog/src/components/index.ts
+++ packages/web/blog/src/components/index.ts
@@ -7,7 +7,6 @@ export { default as Layout } from './Layout';
 export { default as ListItem } from './ListItem';
 export { default as ContentListItem } from './ContentListItem';
 export { default as SeriesCard } from './SeriesCard';
-export { default as ContentCard } from './ContentCard';
 export { default as Description } from './mdx/Description';
 export { default as Toggle } from './mdx/Toggle';
 export { default as CodeBlock } from './CodeBlock';
